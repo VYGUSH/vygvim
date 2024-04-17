6dfa4e2205782d079d83b0bd960b92def0532e85
V G <vyg@MacBook-Pro-V.local>
2024-04-17 22:44:42 +0300

    add plugin neo-tree

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -13,4 +13,14 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+            "s1n7ax/nvim-window-picker"
+	}
+    },
 })
